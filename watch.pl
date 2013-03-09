e2b1eb03431ff264ebb6ab29b89d5b012f1441d9
Johan Lindstrom <jpl@nospam>
2013-03-09 20:47:49 +0000

    watch.pl --initial-run

--- watch.pl
+++ watch.pl
@@ -5,19 +5,25 @@ use Time::HiRes qw/ sleep /;
 
 main();
 sub main {
-    GetOptions( "run:s" => \( my $run ) );
+    GetOptions(
+        "run:s"       => \( my $run ),
+        "initial-run" => \( my $initial_run ),
+    );
     my @files = @ARGV;
     @files && $run or die("Syntax: $0 --run=COMMAND FILES
 
 Watch the FILES and whenever any of them changes, run COMMAND \$FILE
 for each of them.");
 
-    my $file_stats = get_stats(@files);
+    my $file_stats = $initial_run
+        ? { }
+        : get_stats(@files);
+
     while(1) {
         my $file_new_stats = get_stats(@files);
 
         my @files_to_run =
-            grep { $file_new_stats->{$_} ne $file_stats->{$_} }
+            grep { $file_new_stats->{$_} ne ($file_stats->{$_} || 0) }
             @files;
         for my $file (@files_to_run) {
             print "$run $file\n";
